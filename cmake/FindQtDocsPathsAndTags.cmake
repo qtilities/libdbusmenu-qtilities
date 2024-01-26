bd5c6a3d43a358067d0d988d5745d7a2cce2b2d8
redtide <redtid3@gmail.com>
2024-01-26 14:31:20 +0100

    FindQtDocsPathsAndTags.cmake license, minimal changes

--- cmake/FindQtDocsPathsAndTags.cmake
+++ cmake/FindQtDocsPathsAndTags.cmake
@@ -1,34 +1,37 @@
+# SPDX-FileCopyrightText: 2024 Paul Colby <git@colby.id.au>
+# SPDX-License-Identifier: BSD-3-Clause
+
 # Attempt to detect the QT_INSTALL_DOCS path via qtpaths, or qmake.
 if(NOT QT_INSTALL_DOCS)
     if(QT_VERSION VERSION_GREATER_EQUAL 6.2.0 AND TARGET Qt${QT_VERSION_MAJOR}::qtpaths)
         message(DEBUG "Fetching location of imported Qt${QT_VERSION_MAJOR}::qtpaths command")
-        get_target_property(COMMAND Qt${QT_VERSION_MAJOR}::qtpaths LOCATION)
+        get_target_property(_queryCommand Qt${QT_VERSION_MAJOR}::qtpaths LOCATION)
     elseif(TARGET Qt${QT_VERSION_MAJOR}::qmake)
         message(DEBUG "Fetching location of imported Qt${QT_VERSION_MAJOR}::qmake command")
-        get_target_property(COMMAND Qt${QT_VERSION_MAJOR}::qmake LOCATION)
+        get_target_property(_queryCommand Qt${QT_VERSION_MAJOR}::qmake LOCATION)
     else()
         message(STATUS "Neither Qt${QT_VERSION_MAJOR}::qtpaths nor Qt${QT_VERSION_MAJOR}::qmake available")
     endif()
 
-    if(COMMAND)
-        message(VERBOSE "Fetching QT_INSTALL_DOCS from ${COMMAND}")
+    if(_queryCommand)
+        message(VERBOSE "Fetching QT_INSTALL_DOCS from ${_queryCommand}")
         execute_process(
-            COMMAND "${COMMAND}" -query QT_INSTALL_DOCS
-            RESULT_VARIABLE COMMAND_RESULT
-            OUTPUT_VARIABLE COMMAND_OUTPUT
-            ERROR_VARIABLE COMMNAD_ERROR
+            COMMAND "${_queryCommand}" -query QT_INSTALL_DOCS
+            RESULT_VARIABLE _commandResult
+            OUTPUT_VARIABLE _commandOutput
+            ERROR_VARIABLE _commandError
             OUTPUT_STRIP_TRAILING_WHITESPACE
             ERROR_STRIP_TRAILING_WHITESPACE
         )
-        message(DEBUG "${COMMAND} result: ${COMMAND_RESULT}")
-        message(DEBUG "${COMMAND} output: ${COMMAND_OUTPUT}")
-        message(DEBUG "${COMMAND} error: ${COMMAND_ERROR}")
+        message(DEBUG "${_queryCommand} result: ${_commandResult}")
+        message(DEBUG "${_queryCommand} output: ${_commandOutput}")
+        message(DEBUG "${_queryCommand} error: ${_commandError}")
 
-        if(COMMAND_RESULT EQUAL 0)
-            set(QT_INSTALL_DOCS "${COMMAND_OUTPUT}")
+        if(_commandResult EQUAL 0)
+            set(QT_INSTALL_DOCS "${_commandOutput}")
             message(STATUS "Found QT_INSTALL_DOCS: ${QT_INSTALL_DOCS}")
         else()
-            message(STATUS "Failed to find QT_INSTALL_DOCS: ${COMMAND_OUTPUT} ${COMMAND_ERROR} [${COMMAND_RESULT}]")
+            message(STATUS "Failed to find QT_INSTALL_DOCS: ${_commandOutput} ${_commandError} [${_commandResult}]")
         endif()
     endif()
 endif()
